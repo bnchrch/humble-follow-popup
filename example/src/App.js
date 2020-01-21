3f8903343a5b6d6418ccbc3bfbf8f680c622c2f2
Ben Church <ben.douglas.church@gmail.com>
2020-01-21 15:32:12 -0700

    Move state to example app

--- example/src/App.js
+++ example/src/App.js
@@ -5,6 +5,24 @@ import withProps from 'recompose/withProps';
 
 import {HumbleFollowModal} from 'humble-follow-popup';
 
+const MESSAGE_TEXT = `
+Hey, thanks so much for reading!
+
+My goal to 2020 is to simply make other developers lives easier. This goal is directly affected by how many people know about what I'm doing.
+
+So if you enjoyed the content so far I would be thrilled if you followed me somewhere, anywhere.
+
+Don't worry though! This is not a shakedown! You can close this pop up. No penalty. and I wont ask again.
+
+Thanks again for reading and I'm always open to DM's so feel free to message me directly anytime.
+`
+
+const SOCIAL_ACCOUNTS = {
+  productHunt: 'https://www.producthunt.com/@bnchrch',
+  twitter: 'https://twitter.com/bnchrch',
+  indieHackers: 'https://www.indiehackers.com/bnchrch/',
+}
+
 const AppPure = ({modalIsOpen, closeModal, openModal, text}) => {
   return (
     <div>
@@ -14,13 +32,16 @@ const AppPure = ({modalIsOpen, closeModal, openModal, text}) => {
         openModal={openModal}
         modalIsOpen={modalIsOpen}
         title='👋 One humble request from an Indie Dev'
+        closeText='Thanks, but no thanks'
+        messageText={MESSAGE_TEXT}
+        socialAccounts={SOCIAL_ACCOUNTS}
       />
     </div>
   )
 }
 
 const withModalState = compose(
-  withState('modalIsOpen', 'setModalOpen', false),
+  withState('modalIsOpen', 'setModalOpen', true),
   withProps(({setModalOpen}) => ({
     closeModal: () => setModalOpen(false),
     openModal: () => setModalOpen(true)
--- src/index.js
+++ src/index.js
@@ -1,18 +1,61 @@
 import React from 'react'
 import Modal from 'react-modal'
-// import styles from './styles.css'
+import styles from './styles.css'
+import map from 'lodash/fp/map';
+const mapWithKey = map.convert({cap: false});
 
-const HumbleFollowModal = ({modalIsOpen, closeModal, afterOpenModal, customStyles, title}) => {
+const Title = ({children}) => <div className={styles.titleContainer}>{children}</div>
+
+const Message = ({messageText, closeModal, closeText}) => (
+  <div className={styles.messageContainer}>
+    <div className={styles.messageBody}>{messageText}</div>
+    <button className={styles.messageButton} onClick={closeModal}>{closeText}</button>
+  </div>
+)
+
+const SocialMediaCTA = ({service, url}) => {
+  console.log({service, url})
+  return (<div className={styles.socialMediaCTAContainer}><a href={url}>{service}</a></div>)
+}
+
+const SocialMediaCTAs = ({socialAccounts}) => {
+  // alert(JSON.stringify(socialAccounts))
+  return (
+    <div className={styles.socialCTAColumn}>
+      {
+        mapWithKey(
+          (value, key) => <SocialMediaCTA key={key} service={key} url={value} />,
+          socialAccounts
+        )
+      }
+    </div>
+  )
+}
+
+const HumbleFollowModal = ({
+  modalIsOpen,
+  closeModal,
+  afterOpenModal,
+  customStyles,
+  title,
+  messageText,
+  socialAccounts,
+  closeText
+}) => {
   return (
     <Modal
       isOpen={modalIsOpen}
       onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
       style={customStyles}
-      contentLabel='Example Modal'
+      contentLabel={title}
     >
 
-      <h2>{title}</h2>
+      <Title>{title}</Title>
+      <div className={styles.body}>
+        <Message messageText={messageText} closeModal={closeModal} closeText={closeText}/>
+        <SocialMediaCTAs socialAccounts={socialAccounts} />
+      </div>
       <button onClick={closeModal}>Close</button>
     </Modal>)
 }

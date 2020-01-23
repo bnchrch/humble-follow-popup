c41ceb7c7f8901f81d21fc8a93444475c10d3b2e
Ben Church <ben.douglas.church@gmail.com>
2020-01-23 15:10:55 -0700

    Get widget export working with parcel

--- widget/index.js
+++ widget/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import HumbleFollow from 'humble-follow-popup'
-console.log('here')
 
 function initHumbleFollow (props) {
-  console.log('called')
-
   ReactDOM.render(<HumbleFollow {...props} />, document.getElementById('root'))
-  return this
 }
 
-export default initHumbleFollow
+(() => {
+  window.HumbleFollow = initHumbleFollow
+})()
 
-// export default 'Hello, world!'

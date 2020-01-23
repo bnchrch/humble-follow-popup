45a26accfb9bf2fd5e404374e28c6b1acb64a468
Ben Church <ben.douglas.church@gmail.com>
2020-01-23 15:22:36 -0700

    Remove rollup BS

--- widget/widget.js
+++ widget/widget.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import HumbleFollow from 'humble-follow-popup'
 
 function initHumbleFollow (props) {
-  ReactDOM.render(<HumbleFollow {...props} />, document.getElementById('root'))
+  const widgetElement = document.createElement('div')
+  document.body.appendChild(widgetElement)
+  ReactDOM.render(<HumbleFollow {...props} />, widgetElement)
 }
 
 (() => {
   window.HumbleFollow = initHumbleFollow
 })()
-
